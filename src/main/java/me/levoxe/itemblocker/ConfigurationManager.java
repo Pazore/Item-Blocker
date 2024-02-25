616f4790d1493ce74431e2e89ea6502afa71aef6
Avelify <pazoree@gmail.com>
2024-02-25 14:03:16 -0500

    Plugin

--- src/main/java/me/levoxe/itemblocker/ConfigurationManager.java
+++ src/main/java/me/levoxe/itemblocker/ConfigurationManager.java
@@ -10,17 +10,14 @@ public class ConfigurationManager {
         this.config = config;
     }
 
-    // Method to check if an item is blocked
     public boolean isItemBlocked(String itemName) {
         return config.getStringList("blockedItems").contains(itemName);
     }
 
-    // Method to get the permission required for an item
     public String getItemPermission(String itemName) {
         return config.getString("permissions." + itemName);
     }
 
-    // Method to get the no-permission message from the config
     public String getNoPermissionMessage() {
         return config.getString("no-permission");
     }
